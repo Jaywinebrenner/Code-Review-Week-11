02b14d6a17c45127d9d95f4d7b4ae46d3c7ac2e9
Jay Winebrenner <jaywinebrenner@gmail.com>
2020-01-24 09:49:04 -0800

    rework root path view to have nav bar on top

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users
-  #   root to: 'products#index'
-  # resources :products do
+    root to: 'products#index'
+  resources :products do
   #   resources :reviews
   end
-# end
+end
 
 
 #
